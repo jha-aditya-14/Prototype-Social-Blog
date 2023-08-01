132cc8a3181f2685e76f8ca8a7a4cdb926c0dced
jha-aditya14 <137380998+jha-aditya14@users.noreply.github.com>
2023-08-01 15:41:19 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vg8j8h#c23#$744+7^rg)01vito!#ss6+bqxze4zwp5&0jbvu_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
